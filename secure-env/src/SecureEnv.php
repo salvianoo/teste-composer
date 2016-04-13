a21b56eb9be9ae5e1c2b50bc4571f1da9b86b5ae
Salviano Ludgerio <salviano@fretebras.com.br>
2016-04-13 14:11:58 -0300

    Minor

--- secure-env/src/SecureEnv.php
+++ secure-env/src/SecureEnv.php
@@ -15,8 +15,8 @@ class SecureEnv
     {
         if (isset($this->secEnv[$name])) {
             return trim($this->secEnv[$name]);
-        } elseif (getenv($name) !== false) { // usar
-            return trim(getenv($name));
+        } elseif ($_SERVER[$name] !== false) { // usar
+            return trim($_SERVER[$name]);
         } else {
             return $fallback;
         }
@@ -24,11 +24,17 @@ class SecureEnv
 
     private function bootstrapSecEnv($key)
     {
-        foreach ($this->envArray() as $name => $value) {
+        foreach ($_SERVER as $name => $value) {
             if (is_string($value) && strpos($value, 'ENC:') === 0) {
                 $this->secEnv[$name] = $this->decryptEnv($key, substr($value, 4));
             }
         }
+
+        // foreach ($this->envArray() as $name => $value) {
+        //     if (is_string($value) && strpos($value, 'ENC:') === 0) {
+        //         $this->secEnv[$name] = $this->decryptEnv($key, substr($value, 4));
+        //     }
+        // }
     }
 
     private function decryptEnv($key, $encoded)
@@ -37,19 +43,19 @@ class SecureEnv
         return mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key, $value, MCRYPT_MODE_CBC, $iv);
     }
 
-    private function envArray()
-    {
-        $vars_env = explode("\n", shell_exec('printenv'));
+    // private function envArray()
+    // {
+    //     $vars_env = explode("\n", shell_exec('printenv'));
 
-        // print_r($vars_env);
+    //     // print_r($vars_env);
 
 
-        return array_reduce($vars_env, function ($acc, $value) {
-            $var_env = (strstr($value, '=') ? explode("=", $value) : array($value, ''));  // FIX THIS =
+    //     return array_reduce($vars_env, function ($acc, $value) {
+    //         $var_env = (strstr($value, '=') ? explode("=", $value) : array($value, ''));  // FIX THIS =
 
-            // print_r($var_env);
+    //         // print_r($var_env);
 
-            return ( array_merge($acc, array($var_env[0] => $var_env[1])) );
-        }, []);
-    }
+    //         return ( array_merge($acc, array($var_env[0] => $var_env[1])) );
+    //     }, []);
+    // }
 }
